725849596e21edf58d65ce60eaec99b0c89233f7
lmhufnagel <lmhufnagel@gmail.com>
2018-02-11 20:59:19 -0500

    user reducer created with add-trip

--- src/reducers/userReducer.js
+++ src/reducers/userReducer.js
@@ -1,11 +1,24 @@
-export function userReducer(state = {
-  users: [],
-}, action) {
+// export function userReducer(state = {
+//   users: [],
+// }, action) {
+//
+//   switch(action.type) {
+//     case "FETCH_USER":
+//       return {...state, user: action.payload}
+//     default:
+//       return state
+//   }
+// }
 
+
+export function userReducer(state = {currentUser: {}}, action) {
   switch(action.type) {
-    case "FETCH_USER":
-      return {...state, user: action.payload}
+
+    case "ADD_TRIP":
+      return {...state, currentUser: {...state.currentUser, trips: [...state.currentUser.trips, action.payload]}}
+
     default:
-      return state
+      return {...state}
   }
+
 }
